5492353cc112a2c83f5fc7444e184fbd4291b5fd
remzicelebi <remzicelebi@73f19a22-2f05-11df-82ed-5f3c927ac887>
2010-07-26 13:15:11 +0000

    Distributed working version...

--- trunk/master.py
+++ trunk/master.py
@@ -3,33 +3,43 @@ import simulation
 
 
 class Master:
-	def __init__(self):
+	def __init__(self, sects, allhabitats):
 		self.ntasks = simulation.comm.Get_size()
 		self.slaves = range(1,self.ntasks)
-		self.loc2ag = {}
+		self.sect2hab = sects
+		self.allhabitats = allhabitats
 		self.totaldefnum = 0
 		self.totalcoopnum = 0
+	 	self.habid2agentsadded = dict()
 		step = 0
-		
-		while(True):
+		iteration = 1
+		while(iteration  < simulation.ITERATION_NUM ):
 			if step == 0:
 				print "master---step 0"
 				self.startTournament()
 				step=step+1
 			elif step == 1:	
 				print "master---step 1"
+				self.habid2agentsadded = {}
 				for rank in range(1, self.ntasks):
 					self.recieveTournamentEndInfo(rank)
 				print "Number of defects %d and number of cooperates %d"%(self.totaldefnum,self.totalcoopnum) 	
-				step= step +1
+				step=step +1
 			elif step == 2:
-				#print "good"
-				for rank in range(1,self.ntasks):
-					print "good"
-					#self.doMigration(agents, habitatId, slaveId)
+				print "master---step 2"
+				for sid,habs in self.sect2hab.items():
+					sectainfo = {}
+					for habid in habs:
+						if self.habid2agentsadded.has_key(habid):
+							sectainfo[self.habid2agentsadded[habid]] = habid
+					self.doMigration(sectainfo, sid)
 				step= step +1
-				
-
+			elif step == 3:
+				self.waitAcknowledge()
+				step = 0
+			iteration += 1
+	
+	print "*******FINISH****** MASTER"
 
 	"""
 	boardcast init msg to all slaves
@@ -45,19 +55,25 @@ class Master:
 		- from which habitat to where
 		- each habitat' scores in the slave
 		- habitat cooperate/ defect numbers
+
+
+ 		- agents to be moved between slaves (list)
+		- its current slave-id
+		- agent-new habitat location pair   (map)
+		- each habitat' total score
+		- each habitat' cooperates/defects number
 	"""
 	def recieveTournamentEndInfo(self,rank):
 
-		data = simulation.comm.recv(source=rank,tag = simulation.MSG_MOVE_TYPE)
+		data = simulation.comm.recv(source=rank,tag = simulation.MSG_TOURNAMENT_END_TYPE)
 
-		if data is not None: return
+		#print "master data", data
+		if data is None: return
 
-		if data["agents"] is not None:
-			agents = data["agents"]
 		if data["slaveId"] is not None:
 			slaveId= data["slaveId"]
-		if data["locations"] is not None:
-			locationsTobeMoved = data["locations"]
+		if data["agentstransfered"] is not None:
+			agentstransfered = data["agentstransfered"]
 		if data["score"] is not None:	
 			totalScore = data["score"]
 		if data["coopNum"] is not None:
@@ -68,33 +84,32 @@ class Master:
 		self.totaldefnum += coopNum
 		self.totalcoopnum += defNum
 
-		print "RECEIVED- form slave %d cooperate num: %d defect num: %d"%(slaveId,coopNum,defNum)
-		
-		for ag,loc in locations:
-			if self.loc2ag.__contains__(loc):
-				self.loc2ag[loc].append(ag)
-			else:
-				self.loc2ag[loc] =[ag]
-		
-	
-	"""
-	 send Movement msg to related slaves:
-		- set of agents
-		- moved location (habitat ids)	
-	"""
-	def doMigration(self, agents, habitatId, slaveId):
+		print "RECEIVED- from slave %d cooperate num: %d defect num: %d"%(slaveId,coopNum,defNum)
 
+		for agent,hid in agentstransfered.items():
+			if not self.habid2agentsadded.has_key(hid):
+				 self.habid2agentsadded[hid] = [agent]
+			else:
+				self.habid2agentsadded[hid].append(agent)
+
+
+	def doMigration(self, sectainfo, sid):
+		print "send doMigration"
 		data = dict()
-		if agents is not None:
-			data["agents"] = agents
-		if locationsThatAgentsMoved is not None:
-			data["habitatId"] = habitatId
+		if sectainfo is not None:
+			data["sectionaddinfo"] = sectainfo
+
 		
-		simulation.comm.send(data, slaveId,tag = simulation.MSG_MOVE_TYPE)
+		simulation.comm.send(data, dest = sid, tag = simulation.MSG_MOVE_TYPE)
+		
 		
 
 	"""
 	after migration, gather all ack msgs from related slaves (not from all slaves) 
 	"""
-	def waitAcknowledge(self):	
-		print "not implemented"
+	def waitAcknowledge(self):
+		for rank in self.slaves:	
+			data = simulation.comm.recv(source=rank,tag = simulation.MSG_END_TYPE)
+
+
+
--- trunk/slave.py
+++ trunk/slave.py
@@ -14,86 +14,50 @@ Slave consits of a group of habitats, responsible for execution of interactions 
 habitats
 """
 class Slave:
-	def __init__(self, slaveId, numRow, numCol, sdfMatrix, numAgents, habitatids, habitatNeighMap):
+	def __init__(self, slaveId, habitats, habitatNeighMap, hab2sfd):
 		self.masterId = 0
 		self.slaveId = slaveId
 		self.totalScore = 0
-		self.numRow = numRow
-		self.numCol = numCol
-		self.sdfMatrix =sdfMatrix
-		self.numAgents = numAgents
 		self.habitatNeighborMap = habitatNeighMap
-		self.habitats = {}
+		self.habitats = habitats
 		self.step = 0
 		self.agentsremovedfromhab ={}
 		self.agentsaddedtohab = {}	
 		self.totalAgentNum = 0
 		self.totalDefNum = 0
 		self.totalCoopNum = 0
-		self.agentsTobeMoved=[]
-		
-		self.createHabitats(self.numRow, self.numCol, self.sdfMatrix, numAgents, habitatids)			
-
+		self.hab2sfd = hab2sfd
+			
+		iteration = 1
 		step = 0
-		while(True):
+		while( True ):
 			if step == 0:
 				print "slave---step 0"
 				data = simulation.comm.recv(source=self.masterId,tag = simulation.MSG_INIT_TYPE)
 				print "data"
 				print data
-				self.startTournament(step)
+				self.startTournament(iteration)
 				step= step +1
-			elif step == 1:	
-				self.sendTournamentEndInfoMsg( self.agentsTobeMoved, self.slaveId, self.agentsaddedtohab, self.totalScore, self.totalCoopNum, self.totalDefNum)
+			elif step == 1:
+				agentstransfered = {}					
+				for agent,habid in self.agentsaddedtohab.items():
+					if habid not in hab2sfd.keys():
+						agentstransfered[agent] = habid
+
+				self.sendTournamentEndInfoMsg(self.slaveId, agentstransfered, self.totalScore, self.totalCoopNum, self.totalDefNum)
+				print "Total moves : %d"%(len(  self.agentsaddedtohab ))
+				self.moveAgents(self.agentsremovedfromhab, self.agentsaddedtohab)
 				step= step +1
 			elif step == 2:
 				self.recieveDoMovementMsg()
 				step= step +1
-				
+			elif step == 3:
+				self.sendMovementEndAck()
+				step = 0
+			iteration +=1	
 
-				 
-				
+		print "*******FINISH****** SLAVE"		 
 
-	def createHabitats(self, numRow, numCol, sdfMatrix, numAgents, habitatids=None):
-		global nextId
-		nextId =1 
-		timeSteps = numAgents - simulation.SCALEFREE_INIT_NODES_NUM
-		topo = scalefree.ScaleFree(simulation.SCALEFREE_INIT_NODES_NUM, simulation.SCALEFREE_EDGES_NUM_TO_ATTACH, timeSteps)
-		intmat = topo.getInterconnMatInt()
-		
-		for i in range(numRow):
-			for j in range(numCol):
-				node2agent= {}
-				agents = self.createAgents(numAgents)
-				k=0
-				for node in topo.nodes:
-					node2agent[node.label] = agents[k].id
-					k+=1						
-				aintmat = []		
-				for e in intmat:
-					aintmat.append([node2agent[e[0]], node2agent[e[1]]])
-				
-				#print intmat
-				#print agentids
-				if habitatids == None:
-					habid = ++nextId
-				else:
-					habid = habitatids[i*numCol +j]
-				self.habitats[habid] = habitat.Habitat(habid, agents, aintmat, sdfMatrix[i][j])					
-					
-		return self.habitats
-
-	def createAgents(self, numAgents):
-		agents=[]
-		for i in range(numAgents):
-			if random.random() < simulation.PROB_COOPERATE:
-				strategy = simulation.STRATEGY_COOPERATE
-			else:
-				strategy = simulation.STRATEGY_DEFECT
-			agents.append(habitat.Agent(strategy))			
-		return agents
-	
-	
 	"""
 	After recieving signal from Master, start Tournament in its all habitats
 	update scores and define movements
@@ -104,22 +68,15 @@ class Slave:
 
 		times= {}
 		times[0] = time.time()
-		agentsTobeMoved =  []
 		
 		
 		self.totalAgentNum = 0
 		self.totalDefNum = 0
 		self.totalCoopNum = 0
 					
-		for k,habitat in self.habitats.items():
-			self.totalAgentNum+= habitat.agentnum
-			self.totalDefNum+= habitat.defnum
-			self.totalCoopNum+= habitat.coopnum
-		
-		fc = float(self.totalCoopNum) / float(self.totalAgentNum)
-		print "%d. slave:  Number of defects %d and number of cooperates %d"%(self.slaveId,self.totalDefNum,self.totalCoopNum) 
-
 		times[1] = time.time()
+
+		print "%d. slave:  Number of defects %d and number of cooperates %d"%(self.slaveId,self.totalDefNum,self.totalCoopNum) 
 
 		print "%d. slave: Iteration %d"%(self.slaveId,i)
 		print "%d. slave: Logging time : %d"%(self.slaveId, times[1]-times[0])
@@ -151,13 +108,14 @@ class Slave:
 					
 					if random.random() < prob:
 
-						neighbors = [self.habitats[x] for x in  self.habitatNeighborMap[habitat.id]]
-						selhab = self.selectANeighborHabitat(habitat, neighbors)
+						neighborsfds = [ self.hab2sfd[x] for x in  self.habitatNeighborMap[habitat.id]]
+						habids = [ x for x in  self.habitatNeighborMap[habitat.id]]
+						selhab = self.selectANeighborHabitat(habitat, neighborsfds)
 
 						if selhab == None: continue
-						agentsTobeMoved.append(agent)
+		
 						self.agentsremovedfromhab[agent] = habitat.id
-						self.agentsaddedtohab[agent] = selhab.id
+						self.agentsaddedtohab[agent] = habids[selhab]
 
 		#=====================================================================
 		#transfer agents between containers
@@ -169,34 +127,42 @@ class Slave:
 
 		times[4] = time.time()
 		print "%d. slave: Total running time: %d"%(self.slaveId,times[4]-times[0])
+		
+		for k,habitat in self.habitats.items():
+			self.totalAgentNum+= habitat.agentnum
+			self.totalDefNum+= habitat.defnum
+			self.totalCoopNum+= habitat.coopnum
+		if self.totalAgentNum != 0:
+			fc = float(self.totalCoopNum) / float(self.totalAgentNum)
+		else: fc = 0.0		
+		print "%d. slave:  Number of defects %d and number of cooperates %d"%(self.slaveId,self.totalDefNum,self.totalCoopNum) 
+
+
 
 	"""
 	select a neighbor habitat for agents to be transfered 
 	"""				
 	def selectANeighborHabitat(self, habitat, neighbors):
 		selhab = random.randint(0,len(neighbors)-1)
-		prob = 1.0 / (1.0 + math.exp( habitat.diversity - neighbors[selhab].diversity) / simulation.K2VALUE )
+		prob = 1.0 / (1.0 + math.exp( habitat.diversity - neighbors[selhab]) / simulation.K2VALUE )
 		if random.random() < prob:
-			return neighbors[selhab] 
+			return selhab 
 		
 		return None
 
 	"""
  	After tournament edns, it sends to the master the mesagge containing following information:
-		- agents to be moved between slaves (list)
 		- its current slave-id
 		- agent-new habitat location pair   (map)
 		- each habitat' total score
 		- each habitat' cooperates/defects number
 	"""
-	def sendTournamentEndInfoMsg(self, agentsMovedBtwSlaves, mySlaveId, locationsThatAgentsMoved, totalScore, coopNum, defNum):
+	def sendTournamentEndInfoMsg(self, mySlaveId, agentstransfered, totalScore, coopNum, defNum):
 		data = dict()
-		if agentsMovedBtwSlaves is not None:
-			data["agents"] = agentsMovedBtwSlaves
 		if mySlaveId is not None:
 			data["slaveId"] = mySlaveId
-		if locationsThatAgentsMoved is not None:
-			data["locations"] = locationsThatAgentsMoved
+		if agentstransfered is not None:
+			data["agentstransfered"] = agentstransfered
 		if totalScore is not None:	
 			data["score"] = totalScore
 		if coopNum is not None:
@@ -204,23 +170,174 @@ class Slave:
 		if defNum is not None:
 			data["defNum"] = defNum
 		#send data to master
-		simulation.comm.send(data, dest = 0,tag= simulation.MSG_MOVE_TYPE)
+		simulation.comm.send(data, dest = 0, tag= simulation.MSG_TOURNAMENT_END_TYPE)
 			
 			
 		
 	
 	"""
 	gets msg having following information:
-		- agents to be moved to new slave 
-			-agents
+		- agents to be removed to new slave 
+			-agents to be added
 			- new locations (slave-ids)
 	"""	
 	def recieveDoMovementMsg(self):
-		print "recieveDoMovementMsg --not implemented"
+		data = simulation.comm.recv(source=self.masterId,tag = simulation.MSG_MOVE_TYPE)
+		agentstransfered ={}
+		if data["sectionaddinfo"] is not None:
+			agentstransfered = data["sectionaddinfo"]
+
+		for agent in agentstransfered:
+			if self.habitats.has_key( agentsAddedToHab[agent] ):  
+				self.addAgentIntoHabitat(agent, self.habitats[agentsAddedToHab[agent]], simulation.SCALEFREE_EDGES_NUM_TO_ATTACH)
+
 
 	"""
 	a dummy msg for acknowledge to indicate tournament in that slave is end
 	"""
-	def sendMovementEndAck():
-		print "sendMovementEndAck --not implemented"
+	def sendMovementEndAck(self):
+		data = "end"
+		simulation.comm.send(data, dest=0, tag= simulation.MSG_END_TYPE)
+
+
+
+	"""
+	remove agent from old habitat and add agent to new habitat
+	"""
+	def moveAgents(self, agentRemovedFromHab, agentsAddedToHab):
+		
+		for agent,habId in agentRemovedFromHab.items():
+
+			if self.habitats.has_key( habId ):
+				self.removeAgentFromHabitat(agent, self.habitats[habId])
+				
+			if self.habitats.has_key( agentsAddedToHab[agent] ):
+				self.addAgentIntoHabitat(agent, self.habitats[ agentsAddedToHab[agent] ], simulation.SCALEFREE_EDGES_NUM_TO_ATTACH)
+			
+	"""
+	do a binary search for the location i such that
+	probabilities [i-1]< val <= probabilites[i]
+	"""
+	def selectProb(self, n, prob):
+		val = random.random()
+		 
+		left =0
+		right = n-1
+		i = 0
+		while right >= left:
+			middle = (left+right)/2
+ 			if (val- prob[middle]) < 0.0:
+				right = middle-1
+			elif (val - prob[middle]) > 0.0:
+				left = middle+1
+			else:
+			 	i = midlle
+				break
+	 		
+		if right < left:
+			i= left
+		# in the case there were neighboring items with probability 0
+		while i > 0 and prob[i-1] == val:
+			i-=1
+
+		return i
+
+	"""
+	while removing agent, to not lose connections of peer from the graph,
+	there is rewiring process
+		- no rewiring required if only it has one or no connections
+		- selecting a node (agent) to transfer all removed one' connection onto it
+			 with some probability based on it degree .
+		- transfer removed connections to selected peer 
+	"""
+
+	def removeAgentFromHabitat(self, agent, habitat):
+		#print "agent %d removed from habitat %d"%(agent.id, habitat.id)
+		agentsConnected = habitat.getConnectedAgents(agent)
+		edgesConnectedToRemoved = habitat.getConnectedEdges(agent)
+
+		agentsConnectedNum = len(agentsConnected)
+		if agentsConnectedNum <= 1:
+			for e in edgesConnectedToRemoved:
+				habitat.removeEdge(e)
+			habitat.removeAgent(agent)
+			return True
+			
+		prob=[]
+		totalDegree= float( sum(a.outdegree for a in agentsConnected))
+		prob.append( float(agentsConnected[0].outdegree)/ totalDegree )
+		for i in range(1, agentsConnectedNum):
+			prob.append( prob[i-1] + float(agentsConnected[i].outdegree)/totalDegree )
+
+		
+		selected = self.selectProb(agentsConnectedNum, prob)
+		selAgent = agentsConnected[selected]
+		
+		while selAgent == agent:
+			selected = self.selectProb(agentsConnectedNum, prob)
+			selAgent = agentsConnected[selected]
+		# remove agent and rewires its connections with a selected connected agent.
+		
+		
+		edgesTobeAdded = [] 
+		for edge in edgesConnectedToRemoved:
+			if agent.id == edge[1]	and selAgent.id != edge[0]:
+				newedge = [edge[0], selAgent.id]
+				edgesTobeAdded.append(newedge)
+			elif agent.id == edge[0] and selAgent.id != edge[1]:
+				newedge = [selAgent.id, edge[1]]				
+				edgesTobeAdded.append(newedge)
+	
+		# remove agent and its connections
+		for e in edgesConnectedToRemoved:
+			habitat.removeEdge(e)
+		habitat.removeAgent(agent)
+
+		edgesConnectedToSelected = habitat.getConnectedEdges(selAgent)
+		# connect edges to selected agent if there is no connection between these nodes 
+		for edge in edgesTobeAdded:
+			crossedge = [edge[1],edge[0]]
+			if edge not in edgesConnectedToSelected and crossedge not in edgesConnectedToSelected:
+				habitat.addEdge(edge)
+		
+		return True	
+
+	def createRandomEdge(self, numAgents, agents, newAgent, prob, totalDegre):	
+		attachPoint = self.selectProb(numAgents, prob)
+		attachAgent = agents[ attachPoint ]
+		return [newAgent.id, attachAgent.id]
+
+	def addAgentIntoHabitat(self, newAgent, habitat, numConn):
+		#print "agent %d added from habitat %d"%(newAgent.id, habitat.id)
+		numAgents = len( habitat.agents ) 
+		if numAgents == 0:
+			habitat.addAgent(newAgent)
+			return True
+		
+		if numAgents == 1:
+			habitat.addAgent(newAgent)
+			habitat.addEdge([newAgent.id,habitat.agents[0].id])
+			return True
+		
+		if numConn > numAgents:	numConn = numAgents	
+			
+		totalDegree = float(habitat.totalOutDegree)
+		prob = []
+		prob.append( float(habitat.agents[0].outdegree)/ totalDegree)
+		for i in range(1, len(habitat.agents)):
+			prob.append( prob[i-1] + float(habitat.agents[i].outdegree)/totalDegree )
+
+		edges = []
+		for k in range(1,numConn):
+			edge = self.createRandomEdge(numAgents, habitat.agents, newAgent, prob, totalDegree)
+			if edge in edges or [edge[1], edge[0]] in edges:
+				k-=1
+			else: edges.append(edge)
+
+		habitat.addAgent(newAgent)
+		
+		for e in edges:
+			habitat.addEdge(e)
+
+		return True
 
